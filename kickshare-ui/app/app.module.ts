6b1a65d8e56cfba3f52366c79e2cb13b86bfd058
Jan Kucera <kucera.jan.cz@gmail.com>
2017-03-24 20:15:58 +0100

    Angular 2 prototype
     Added:
         Angular 2 hello world example
         Google maps simple HTML + JS example

---
+++ kickshare-ui/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HelloWorldComponent }   from './hello-world.component';
+
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ HelloWorldComponent ],
+  bootstrap:    [ HelloWorldComponent ]
+})
+export class AppModule { }
