c0adda493a22cdf8e014643c859a98d2994899db
Jan Kucera <kucera.jan.cz@gmail.com>
2017-03-25 14:09:38 +0100

    Google Maps REST support
     Added:
         GeoJSON and JSONP support for sending GEO coordinates to Google Maps

--- kickshare-rest/src/main/java/com/github/kickshare/security/ApplicationSecurityAdapter.java
+++ kickshare-rest/src/main/java/com/github/kickshare/security/ApplicationSecurityAdapter.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * @author Jan.Kucera
@@ -15,6 +17,7 @@ public class ApplicationSecurityAdapter extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/system/info").anonymous()
+                .antMatchers("/groups/**").anonymous()
                 .anyRequest().authenticated();
         http.httpBasic();
     }
