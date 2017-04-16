441c8187e009dbe0295cc71694b001aa785ef329
Jan Kucera <kucera.jan.cz@gmail.com>
2017-04-16 12:28:37 +0200

    Repository prototype
    Added:
         system property for multiple schemas
         switched DB schema to PostgreSQL

--- kickshare-rest/kickshare-rest/src/main/java/com/github/kickshare/db/multischema/FlywayMultiTenantMigration.java
+++ kickshare-rest/kickshare-rest/src/main/java/com/github/kickshare/db/multischema/FlywayMultiTenantMigration.java
@@ -15,36 +15,24 @@ public class FlywayMultiTenantMigration {
 
     private Supplier<List<String>> schemaProvider;
 
-
     private Flyway flyway;
-    private Flyway adminFlyway;
 
     public FlywayMultiTenantMigration(Flyway flyway, Supplier<List<String>> schemaProvider) {
         this.flyway = flyway;
         this.schemaProvider = schemaProvider;
-        this.adminFlyway = flyway;
     }
 
     @PostConstruct
     void migrate() {
-        migrateAdminSchema();
         migrateAllSchemas();
     }
 
     private void migrateAllSchemas() {
         List<String> schemas = schemaProvider.get();
         for (String schema : schemas) {
-            if (adminFlyway.getSchemas().length > 0
-                    && !adminFlyway.getSchemas()[0].equals(schema)) {
-                flyway.setSchemas(schema);
-                flyway.migrate();
-            }
+            flyway.setSchemas(schema);
+            flyway.migrate();
         }
     }
-
-    private void migrateAdminSchema() {
-        adminFlyway.migrate();
-    }
-
 
 }
