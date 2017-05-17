4c0df4699eebf3f0e455600711f28d66e4cd87fc
Jan Kucera <kucera.jan.cz@gmail.com>
2017-05-17 23:22:05 +0200

    Stress test prototype
    Added:
        gatling dependency
        gatling prototype for calling Kickshare endpoint

---
+++ kickshare-tools/stress-test/src/main/scala/com/github/kickshare/gatling/BasicItSimulation.scala
@@ -0,0 +1,27 @@
+package com.github.kickshare.gatling
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+/**
+  * @author Jan.Kucera
+  * @since 4.5.2017
+  */
+
+
+class BasicItSimulation extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://localhost:8080") // Here is the root for all relative URLs
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+
+  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
+    .exec(http("Search project")
+      .get("/projects/search?name=Quodd+Heroes&categoryId=34"))
+    .pause(2) // Note that Gatling has recorder real time pauses
+
+  setUp(scn.inject(atOnceUsers(3)).protocols(httpConf))
+}
