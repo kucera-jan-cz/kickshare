51144987fb67b29963bfb7dc565049c35c33d9ef
Jan Kucera <kucera.jan.cz@gmail.com>
2017-11-15 22:17:16 +0100

    REST support - external services
    Fixed:
        HtmlRewardService is now able to parse currencies and parsing HTML is easier through HTMLCleaner

--- kickshare-rest/kickshare-ext-service/src/main/java/com/github/kickshare/ext/service/kickstarter/reward/Price.java
+++ kickshare-rest/kickshare-ext-service/src/main/java/com/github/kickshare/ext/service/kickstarter/reward/Price.java
@@ -3,12 +3,14 @@ package com.github.kickshare.ext.service.kickstarter.reward;
 import java.util.Currency;
 
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 /**
  * @author Jan.Kucera
  * @since 14.11.2017
  */
 @AllArgsConstructor
+@ToString
 public class Price {
     private Double amount;
     private Currency currency;
