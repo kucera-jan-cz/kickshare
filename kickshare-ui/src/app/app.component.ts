3cb140b9ef1c2c5c1b03d98cefe916229a04273c
Jan Kucera <kucera.jan.cz@gmail.com>
2018-01-04 20:51:41 +0100

    Front end - application routing
    Removed:
        layout-routing.module and move application routing to app-routing.module with country switching

--- kickshare-ui/src/app/app.component.ts
+++ kickshare-ui/src/app/app.component.ts
@@ -3,6 +3,7 @@ import {LoggerFactory} from "./components/logger/loggerFactory.component";
 
 @Component({
     selector: 'app-root',
+    //@TODO - maybe place <router-outlet></router-outlet> here since it's trivial
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
--- kickshare-ui/src/app/layout/layout.module.ts
+++ kickshare-ui/src/app/layout/layout.module.ts
@@ -1,19 +1,18 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {NgbAlertModule, NgbCollapseModule, NgbDropdownModule} from "@ng-bootstrap/ng-bootstrap";
-
-import {LayoutRoutingModule} from "./layout-routing.module";
 import {LayoutComponent} from "./layout.component";
 import {HeaderComponent, SidebarComponent} from "../components/";
 import {UserService} from "../services/user.service";
 import {AuthGuardService} from "../services/auth-guard.service";
 import {SystemService} from "../services/system.service";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
     imports: [
         CommonModule,
         NgbDropdownModule.forRoot(),
-        LayoutRoutingModule,
+        RouterModule,
         NgbCollapseModule.forRoot(),
         NgbAlertModule.forRoot()
     ],
